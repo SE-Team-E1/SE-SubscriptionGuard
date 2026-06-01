d38baae23eeff6fdb1b4eda12e212a06ae842461
jannik1020 <jannik.thiele.official@gmail.com>
2026-06-01 20:48:05 +0200

    initialized vuejs starter project and added vuetify bottom navigation bar

---
+++ src/walking_skeleton/src/main.ts
@@ -0,0 +1,34 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
+import App from './App.vue'
+import router from './router'
+import {mdi} from "vuetify/iconsets/mdi-svg";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    sets: {
+      mdi,
+    },
+  },
+})
+
+const app = createApp(App)
+
+app.use(vuetify)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
