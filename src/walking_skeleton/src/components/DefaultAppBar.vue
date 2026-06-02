0821913aaef2fc23044f4a2bee9cd9f252718d18
jannik1020 <jannik.thiele.official@gmail.com>
2026-06-02 20:05:17 +0200

    feat: implemented a default top app bar component

---
+++ src/walking_skeleton/src/components/DefaultAppBar.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type {DefaultAppBarProps} from "@/components/types";
+
+const props = defineProps<DefaultAppBarProps>()
+
+</script>
+
+<template>
+  <v-app-bar
+    :extension-height="$slots.extension ? 110: 48"
+    elevation="1"
+  >
+    <v-app-bar-title>{{props.title}}</v-app-bar-title>
+    <template v-if="$slots.extension" #extension>
+      <slot name="extension" />
+    </template>
+  </v-app-bar>
+</template>
+
+<style scoped>
+.v-app-bar-title {
+  margin-inline-start: 0px;
+}
+.v-app-bar {
+  padding: 0px var(--horizontal-screen-padding);
+}
+
+</style>
