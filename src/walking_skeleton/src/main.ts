adfbcffde99bd4756a6e133b2648d6934e03bc93
jannik1020 <jannik.thiele.official@gmail.com>
2026-06-02 20:03:04 +0200

    feat: enable use of icon aliases

--- src/walking_skeleton/src/main.ts
+++ src/walking_skeleton/src/main.ts
@@ -9,16 +9,17 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 
 import App from './App.vue'
 import router from './router'
-import {mdi} from "vuetify/iconsets/mdi-svg";
 
 const vuetify = createVuetify({
   components,
   directives,
   icons: {
     defaultSet: 'mdi',
+    aliases,
     sets: {
       mdi,
     },
