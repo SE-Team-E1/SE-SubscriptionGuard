b0251874d25f7cb99de133f54629a94d06c1ebda
Driftfich WX <fragfranz2000@gmx.de>
2026-06-08 22:27:20 +0200

    feat: Add model files, nearly matching the domain model and considered tech stack/features (OCC, ratings)

---
+++ src/walking_skeleton/src/model/subscriptionModel.ts
@@ -0,0 +1,59 @@
+export enum Currency {
+    EUR = "EUR",
+    USD = "USD",
+    GBP = "GBP",
+}
+
+export interface Payment {
+    id: string,
+    subscriptionId: string,
+    amount: number,
+    date: string,
+    currency: Currency,
+    // version: number, // for OCC sync
+}
+
+export interface Provider {
+    id: string,
+    name: string,
+    url: string
+    // version: number, // for OCC sync
+}
+
+export interface Category {
+    id: string,
+    // userId: string, // not needed for now as just user related data will be fetched. Interesting for multi-user, maybe for sync needed
+    label: string,
+    icon: string,
+    // version: number, // for OCC sync
+}
+
+export enum DeliveryStatus {
+    PENDING = "PENDING",
+    DELIVERED = "DELIVERED",
+    FAILED = "FAILED",
+}
+
+export interface Reminder {
+    id: string,
+    subscriptionId: string,
+    content: string,
+    dueDate: string,
+    deliveryStatus: DeliveryStatus,
+    // version: number, // for OCC sync
+}
+
+export interface Subscription {
+    id: string,
+    // userId: string, // not needed for now as just user related data will be fetched. Interesting for multi-user, maybe for sync needed
+    name: string,
+    price: number,
+    currency: Currency,
+    renewal: string, // billing cycle e.g. 1 month, 1 year, etc.
+    bookingDate: string,
+    cancellationPeriod: string, // time before contractEndDate to cancel the subscription
+    contractEndDate: string,
+    providerId: string,
+    categoryIds: string[],
+    // version: number, // for OCC sync
+}
