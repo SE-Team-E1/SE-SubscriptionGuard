098cd635ada8e2f45bebd9e167fb875a318d7759
jannik1020 <jannik.thiele.official@gmail.com>
2026-06-09 22:45:21 +0200

    feat: added infrastructure repository layer to abstract from data source.

---
+++ src/walking_skeleton/src/repository/SubscriptionRepository.ts
@@ -0,0 +1,147 @@
+import type {CreateSubscriptionDTO, SubscriptionListItemDTO} from "@/feature/list/dto/ListDTO.ts";
+import {Currency} from "@/domain/domain.ts";
+
+export interface TempSubscriptionResponse{
+  "id": number,
+  "anbieter": string,
+  "preis": {
+    "amount": string,
+    "currency": "EUR"
+  },
+  "kündigungsfrist_amount"?: number,
+  "kündigungsfrist_unit": "days" | "weeks" | "months" | "years"
+  "buchungsdatum"?: number,
+  "abschlussdatum": string
+}
+
+function mapResponseToDTO(response: TempSubscriptionResponse): SubscriptionListItemDTO{
+  return {
+    id: response.id.toString(),
+    name: response.anbieter, // gibt kein name
+    provider: response.anbieter,
+    price: {
+      amount: Number.parseFloat(response.preis.amount),
+      currency: response.preis.currency as Currency
+    },
+    renewalPeriod: {
+      amount: response.kündigungsfrist_amount ?? 1,
+      unit: response.kündigungsfrist_unit
+    },
+    rating: {
+      value: Math.random() * 5
+    }
+  }
+}
+
+export interface SubscriptionRepository{
+  getSubscriptionList(): Promise<SubscriptionListItemDTO[]>;
+  createSubscription(dto: CreateSubscriptionDTO): Promise<SubscriptionListItemDTO>;
+}
+
+export class RestSubscriptionRepository implements SubscriptionRepository{
+  createSubscription(dto: CreateSubscriptionDTO): Promise<SubscriptionListItemDTO> {
+    return Promise.resolve({
+      id: 'temp-' + Math.random(),
+      name: dto.name,
+      price: dto.price,
+      renewalPeriod: dto.renewalPeriod,
+      provider: dto.provider,
+    });
+  }
+
+  async getSubscriptionList(): Promise<SubscriptionListItemDTO[]> {
+    try {
+      const response = await fetch("/abos/?format=json")
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const result: TempSubscriptionResponse[] = await response.json();
+      return result.map(mapResponseToDTO);
+    }catch (error: any) {
+      console.error(error.message);
+      return Promise.reject(error.message);
+    }
+  }
+}
+
+export class DummySubscriptionRepository implements SubscriptionRepository{
+  createSubscription(dto: CreateSubscriptionDTO): Promise<SubscriptionListItemDTO> {
+    return Promise.resolve({
+      id: 'temp-' + Math.random(),
+      name: dto.name,
+      price: dto.price,
+      renewalPeriod: dto.renewalPeriod,
+      provider: dto.provider,
+    });
+  }
+
+  getSubscriptionList(): Promise<SubscriptionListItemDTO[]> {
+    return Promise.resolve([
+      {
+        id: "0",
+        name: "Spotify Premium",
+        provider: "Spotify",
+        price: {
+          amount: 35,
+          currency: Currency.EUR
+        },
+        renewalPeriod: {
+          amount: 1,
+          unit: "months"
+        },
+        rating: {
+          value: 4.5
+        }
+      },
+      {
+        id: "1",
+        provider: "Fitnessstudio Heinze",
+        renewalPeriod: {
+          amount: 3,
+          unit: "months"
+        },
+        name: "Fitnessstudio",
+        price: {
+          amount: 105,
+          currency: Currency.EUR
+        },
+        rating: {
+          value: 4.0
+        }
+      },
+      {
+        id: "2",
+        provider: "Xbox",
+        renewalPeriod: {
+          amount: 1,
+          unit: "years"
+        },
+        name: "Game Pass",
+        price: {
+          amount: 155,
+          currency: Currency.EUR
+        },
+        rating: {
+          value: 3.0
+        }
+      }
+    ]);
+  }
+}
+
+export class IndexedDBSubscriptionRepository implements SubscriptionRepository{
+  createSubscription(dto: CreateSubscriptionDTO): Promise<SubscriptionListItemDTO> {
+    return Promise.resolve({
+      id: 'temp-' + Math.random(),
+      name: dto.name,
+      price: dto.price,
+      renewalPeriod: dto.renewalPeriod,
+      provider: dto.provider,
+    });
+  }
+
+  getSubscriptionList(): Promise<SubscriptionListItemDTO[]> {
+    return Promise.resolve([]);
+  }
+}
