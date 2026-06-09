21d99a743e777e919db2e7fd7f26f90de56cc4b0
jannik1020 <jannik.thiele.official@gmail.com>
2026-06-09 23:04:46 +0200

    feat: added composable used to create a subscription via the subscriptioin repository

---
+++ src/walking_skeleton/src/feature/add/useSubscriptionCreation.ts
@@ -0,0 +1,26 @@
+import type {CreateSubscriptionDTO} from "@/feature/add/AddDTO.ts";
+import {inject, ref} from "vue";
+import {Currency} from "@/domain/domain.ts";
+import type {SubscriptionRepository} from "@/repository/SubscriptionRepository.ts";
+
+export default function useSubscriptionCreation() {
+  const newSubscription = ref<CreateSubscriptionDTO>({
+    name: "",
+    price: {
+      amount: 0,
+      currency: Currency.EUR
+    },
+    renewalPeriod: {
+      amount: 1,
+      unit: "months"
+    },
+    startDate: "",
+    provider: "",
+    categories: []
+  });
+  const repository = inject<SubscriptionRepository>("subscriptionRepository") as SubscriptionRepository;
+  const submitSubscription = async () => {
+    await repository.createSubscription(newSubscription.value);
+  }
+  return {newSubscription, submitSubscription};
+}
