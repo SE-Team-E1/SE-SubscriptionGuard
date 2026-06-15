dc3e3f57afc23b6afc203657daf937314374a5ca
jannik1020 <jannik.thiele.official@gmail.com>
2026-06-15 20:39:06 +0200

    feat: #107 implemented basic domain models for subscription, provider an category

--- src/walking_skeleton/src/model/subscriptionModel.ts
+++ src/walking_skeleton/src/model/subscriptionModel.ts
@@ -1,55 +1,111 @@
-import type {Currency} from "@/domain/domain.ts";
+import {type Currency, type Price, type RenewalPeriod} from "@/domain/domain.ts";
 
 export interface Payment {
-    id: string,
-    subscriptionId: string,
-    amount: number,
-    date: string,
-    currency: Currency,
-    // version: number, // for OCC sync
-}
-
-export interface Provider {
-    id: string,
-    name: string,
-    url: string
-    // version: number, // for OCC sync
-}
-
-export interface Category {
-    id: string,
-    // userId: string, // not needed for now as just user related data will be fetched. Interesting for multi-user, maybe for sync needed
-    label: string,
-    icon: string,
-    // version: number, // for OCC sync
+  id: string,
+  subscriptionId: string,
+  amount: number,
+  date: string,
+  currency: Currency,
+  // version: number, // for OCC sync
 }
 
 export enum DeliveryStatus {
-    PENDING = "PENDING",
-    DELIVERED = "DELIVERED",
-    FAILED = "FAILED",
+  PENDING = "PENDING",
+  DELIVERED = "DELIVERED",
+  FAILED = "FAILED",
 }
 
 export interface Reminder {
-    id: string,
-    subscriptionId: string,
-    content: string,
-    dueDate: string,
-    deliveryStatus: DeliveryStatus,
-    // version: number, // for OCC sync
+  id: string,
+  subscriptionId: string,
+  content: string,
+  dueDate: string,
+  deliveryStatus: DeliveryStatus,
+  // version: number, // for OCC sync
 }
 
-export interface Subscription {
-    id: string,
-    // userId: string, // not needed for now as just user related data will be fetched. Interesting for multi-user, maybe for sync needed
-    name: string,
-    price: number,
-    currency: Currency,
-    renewal: string, // billing cycle e.g. 1 month, 1 year, etc.
-    bookingDate: string,
-    cancellationPeriod: string, // time before contractEndDate to cancel the subscription
-    contractEndDate: string,
-    providerId: string,
-    categoryIds: string[],
-    // version: number, // for OCC sync
+
+export class Provider {
+  constructor(
+    private readonly _id: string,
+    private _name: string,
+  ) {
+  }
+}
+
+type CategoryIcon = string
+
+export class Category {
+  constructor(
+    private readonly _id: string,
+    private _name: string,
+    private _icon: CategoryIcon
+  ) {
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get icon(): CategoryIcon {
+    return this._icon;
+  }
+}
+
+export class Subscription {
+  private _contractEndDate: Date | null; // null => unlimited subscription
+
+  constructor(
+    private readonly _id: string,
+    private _name: string,
+    private _price: Price,
+    private _renewal: RenewalPeriod,
+    private readonly _bookingDate: Date,
+    private readonly _createdDate: Date,
+    private _provider: Provider,
+    private _categories: Category[],
+    contractEndDate?: Date
+  ) {
+    this._contractEndDate = contractEndDate ?? null;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get price(): Price {
+    return this._price;
+  }
+
+  get renewal(): RenewalPeriod {
+    return this._renewal;
+  }
+
+  get bookingDate(): Date {
+    return this._bookingDate;
+  }
+
+  get createdDate(): Date {
+    return this._createdDate;
+  }
+
+  get contractEndDate(): Date | null {
+    return this._contractEndDate;
+  }
+
+  get provider(): Provider {
+    return this._provider;
+  }
+
+  get categories(): readonly Category[] {
+    return this._categories;
+  }
 }
