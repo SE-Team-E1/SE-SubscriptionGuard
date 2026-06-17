ec512c1f176ed26c4deaa80993e56b8c9cf5651d
ZockerLS <131686059+ZockerLS@users.noreply.github.com>
2026-06-17 14:21:19 +0200

    Added Bootstrap library

--- src/walking_skeleton/src/App.vue
+++ src/walking_skeleton/src/App.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
+import {computed} from 'vue'
 import {RouterView, useRoute} from 'vue-router'
 import {mdiHome, mdiListBoxOutline, mdiPlusCircleOutline} from "@mdi/js";
 import DefaultAppBar from "@/components/DefaultAppBar.vue";
 import ListAppBar from "@/feature/list/components/ListAppBar.vue";
 
 const route = useRoute();
+const hideAppBar = computed(() => route.meta.hideBar === true)
+const hideNavigation = computed(() => route.meta.hideNavigation === true)
 </script>
 
 <template>
   <v-app>
-    <ListAppBar v-if="route.name === 'list'" :title="route.meta.pageTitle" />
-    <DefaultAppBar v-else :title="route.meta.pageTitle">
-    </DefaultAppBar>
+
+    <ListAppBar v-if="!hideAppBar && route.name === 'list'" :title="route.meta.pageTitle" />
+    <DefaultAppBar v-else-if="!hideAppBar" :title="route.meta.pageTitle" />
+
     <v-main>
       <RouterView/>
     </v-main>
-    <v-bottom-navigation grow>
+
+    <v-bottom-navigation v-if="!hideNavigation" grow>
       <v-btn to="/overview">
         <v-icon :icon="mdiHome"></v-icon>
         <span>Overview</span>
