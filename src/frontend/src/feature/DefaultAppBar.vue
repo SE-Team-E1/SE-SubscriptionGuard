d62887113962731ea6e65e91c67f082a7d3f4906
Driftfich WX <fragfranz2000@gmx.de>
2026-06-20 20:23:07 +0200

    feat: Modified app bar - larger headline, hamburger menu, wrap around

--- src/frontend/src/feature/DefaultAppBar.vue
+++ src/frontend/src/feature/DefaultAppBar.vue
@@ -7,6 +7,7 @@ const props = defineProps<DefaultAppBarProps>()
 
 <template>
   <v-app-bar
+    :height="96"
     :extension-height="$slots.extension ? 110: 48"
     elevation="1"
   >
@@ -14,13 +15,30 @@ const props = defineProps<DefaultAppBarProps>()
     <template v-if="$slots.extension" #extension>
       <slot name="extension" />
     </template>
+
+    <template #append>
+      <v-app-bar-nav-icon size="large" />
+    </template>
   </v-app-bar>
 </template>
 
 <style scoped>
 .v-app-bar-title {
   margin-inline-start: 0px;
+  font-size: 2rem;
+  font-weight: 600;
+  line-height: 1.2;
+  padding-block: 12px;
 }
+
+.v-app-bar-title :deep(.v-toolbar-title__placeholder) {
+  white-space: normal;      /* allow multiple lines */
+  overflow: visible;
+  text-overflow: unset;
+  word-break: normal;         /* don't split words mid-word */
+  overflow-wrap: break-word;  /* only break a word if it's too long for the line */
+}
+
 .v-app-bar {
   padding: 0px var(--horizontal-screen-padding);
 }
