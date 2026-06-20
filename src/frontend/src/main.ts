058d4a79a76c4b70df9002c0fa2a1f06512e1840
jannik1020 <jannik.thiele.official@gmail.com>
2026-06-20 11:20:08 +0200

    feat: draft for initialized mocked repositories for development

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -17,11 +17,19 @@ import {
   type CategoryRepository, type ProviderRepository,
   type SubscriptionRepository
 } from "./repository/subscriptions/repositories.ts";
-import {RestSubscriptionRepository} from "@/repository/subscriptions/restRepositories.ts";
+import {
+  type RestCategoryDTO,
+  type RestProviderDTO,
+  type RestSubscriptionDTO,
+  RestSubscriptionRepository
+} from "@/repository/subscriptions/restRepositories.ts";
 import {
   MockedCategoryRepository, MockedProviderRepository,
   MockedSubscriptionRepository
 } from "@/repository/subscriptions/mockedRepositories.ts";
+import {Category, Provider, Subscription} from "@/domain/subscriptionModel.ts";
+import {Currency, RenewalPeriodUnitEnum} from "@/domain/domain.ts";
+import {mapSubscriptionEntityToDTO} from "@/services/subscriptionServices.ts";
 
 const vuetify = createVuetify({
   components,
@@ -37,9 +45,58 @@ const vuetify = createVuetify({
 
 const app = createApp(App)
 
-app.provide<SubscriptionRepository, string>("subscriptionRepository", new MockedSubscriptionRepository());
-app.provide<CategoryRepository, string>("categoryRepository", new MockedCategoryRepository());
-app.provide<ProviderRepository, string>("providerRepository", new MockedProviderRepository());
+function createTestEntities(amount: number): Subscription[] {
+  return [...Array(amount).keys()].map(i => {
+    const provider = Provider.create(`Subscription ${i} GmbH`);
+    const category = Category.create({name: `Category ${i}`, icon: `icon-category-${i}`});
+    const subscription = Subscription.create({
+        name: "Subscription " + i,
+        price: {
+          amount: i * 100,
+          currency: Currency.EUR
+        },
+        categories: [category],
+        provider: provider,
+        bookingDate: new Date(),
+        renewal: {
+          amount: i,
+          unit: RenewalPeriodUnitEnum.MONTHS
+        }
+      }
+    )
+    return subscription;
+  })
+}
+
+function mapTestEntitiesToDTO(subscriptions: Subscription[]): {subscription: RestSubscriptionDTO, provider: RestProviderDTO, categories: RestCategoryDTO[]}[] {
+  return subscriptions.map(s => {
+    return {
+      subscription: mapSubscriptionEntityToDTO(s),
+      provider: {
+        id: s.provider.id.value,
+        name: s.provider.name
+      },
+      categories: s.categories.map(c => ({
+        id: c.id.value,
+        name: c.name,
+        icon: c.icon
+      }))
+    }
+  });
+}
+
+if( import.meta.env.DEV) {
+  const entities = createTestEntities(3);
+  const dtos = mapTestEntitiesToDTO(entities);
+
+  const subRepo = new MockedSubscriptionRepository(dtos.map(dto => dto.subscription));
+  const catRepo = new MockedCategoryRepository(dtos.map(dto => dto.categories).flat());
+  const provRepo = new MockedProviderRepository(dtos.map(dto => dto.provider));
+
+  app.provide<SubscriptionRepository, string>("subscriptionRepository", subRepo);
+  app.provide<CategoryRepository, string>("categoryRepository", catRepo);
+  app.provide<ProviderRepository, string>("providerRepository", provRepo);
+}
 
 
 app.use(vuetify)
